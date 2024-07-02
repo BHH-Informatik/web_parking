5834a4ab621eee907dcd40943385c75a5686e94f
FabianxS <fabian.schmidt020@gmail.com>
2024-07-02 11:19:03 +0200

    add react project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Navbar } from "./components"
+
+
+function App() {
+
+  return (
+    <>
+      Web Parking
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+        Navigation
+    </>
+  )
+}
+
+export default Navbar
